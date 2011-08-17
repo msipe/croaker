d44aef0883ed97b1a493295806e1cb22110049d7
Andy Sipe <ajs.general@gmail.com>
2011-08-17 07:58:19 -0400

    added js tests, test data and pasic parse method'

--- src/croaker/js/croaker.js
+++ src/croaker/js/croaker.js
@@ -1,3 +1,13 @@
 var Croaker = (function () {
+  var parser = (function () {
+    function parseXml(xml) {
+      return $.xml2json(xml);
+    }
 
+    return { parseXml: parseXml };
+  } ());
+
+  return {
+    parser: parser
+  };
 } ());
