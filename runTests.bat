51d6bc0a4c447b15c043d59c4b191315833e0456
Andy Sipe <ajs.general@gmail.com>
2012-03-20 08:37:14 -0400

    cleaned up batch file

--- runTests.bat
+++ runTests.bat
@@ -1,2 +1,1 @@
-java -jar thirdparty\jstestdriver\JsTestDriver-1.3.4.b.jar --server http://localhost:60000 --tests all --config jsTestDriver.conf --captureConsole
---reset
+java -jar thirdparty\jstestdriver\JsTestDriver-1.3.4.b.jar --server http://localhost:60000 --tests all --config jsTestDriver.conf --captureConsole --reset
