d1d7b1ab3653d5027d79062a3139e13f1793abac
Morgan Sipe <morgan.talk@gmail.com>
2012-03-22 12:04:14 -0400

    working on parsing the child functions

--- src/croaker/js/croaker.js
+++ src/croaker/js/croaker.js
@@ -4,11 +4,15 @@ function Parser() {
   function parse(string) {
     var domparser = new DOMParser(), xmlDoc = domparser.parseFromString(string,"text/xml"),   
 	  root = xmlDoc.documentElement;
-	  
-	/* console.log(root.hasAttributes());
-	console.log(root.attributes.length);
-	console.log(root.attributes.item(0).name);
-    console.log(root.attributes.item(0).value); */
+	 
+	if(root.hasChildNodes()) {
+	  console.log(root.hasChildNodes());
+	  console.log(root.childNodes.length);
+	  console.log(root.childNodes.item(0).nodeName);
+      console.log(root.childNodes.item(0).attributes.item(0).value); 
+	  console.log(root.childNodes.item(0).attributes.item(1).name); 
+	  console.log(root.childNodes.item(0).attributes.item(1).value); 
+	}
 	
     var atrs = {};
 	if (root.hasAttributes()) {
@@ -16,9 +20,18 @@ function Parser() {
 	    atrs[root.attributes.item(x).name] = root.attributes.item(x).value;
 	  }
 	}
+	
+	var children = {};
+	
+	if(root.hasChildNodes()) {
+	  for(x=0; x < root.childNodes.length; x++) {
+	    children[root.childNodes.item(x).nodeName] = root.childNodes.item(x);
+	  }
+	}
+	
 	return {
 	   name: root.nodeName,
-	   children: [],
+	   children: children,
 	   attributes: atrs
 	};
   }
--- src/test/parserTest.js
+++ src/test/parserTest.js
@@ -4,7 +4,7 @@ TestCase("Croaker.ParserTest.Tests", {
 	
 	var tree = parser.parse('<?xml version="1.0"?><players></players>');
 	assertThat(tree.name, equalTo('players'));
-	assertThat(tree.children, empty());
+	assertThat(tree.children, object());
 	assertThat(tree.attributes, object());
   },
 
@@ -13,16 +13,17 @@ TestCase("Croaker.ParserTest.Tests", {
 	
 	var tree = parser.parse('<?xml version="1.0"?><players name="bob"></players>');
 	assertThat(tree.name, equalTo('players'));
-	assertThat(tree.children, empty());
+	assertThat(tree.children, object());
 	assertThat(tree.attributes, hasMember('name', 'bob'));
   }, 
   
-  testParseWithTwoAttributes: function() {
+  testParseWithThreeAttributes: function() {
     var parser = new Parser();
 	
 	var tree = parser.parse('<?xml version="1.0"?><players name="bob" age="5" gender="male"></players>');
+	
 	assertThat(tree.name, equalTo('players'));
-	assertThat(tree.children, empty());
+	assertThat(tree.children, object());
 	assertThat(tree.attributes, hasMember('name', 'bob'));
 	assertThat(tree.attributes, hasMember('age', '5'));
 	assertThat(tree.attributes, hasMember('gender', 'male'));
@@ -32,7 +33,9 @@ TestCase("Croaker.ParserTest.Tests", {
     var parser = new Parser();
 	
 	var tree = parser.parse('<?xml version="1.0"?><players name="bob"><stats type="catches" value="3"/></players>');
-  
+    
+	assertThat(tree.children, hasMember('stats', 'pop'));
+	
   },
   
   setUp: function () {
