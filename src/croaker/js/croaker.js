dcc5970d961bc615e737bb10cb7f989104095c75
Andy Sipe <ajs.general@gmail.com>
2012-03-23 07:27:59 -0400

    cleaned up tab usage

--- src/croaker/js/croaker.js
+++ src/croaker/js/croaker.js
@@ -3,37 +3,37 @@ function Parser() {
   
   function parse(string) {
     var domparser = new DOMParser(), xmlDoc = domparser.parseFromString(string,"text/xml"),   
-	  root = xmlDoc.documentElement;
-	 
-	if(root.hasChildNodes()) {
-	  console.log(root.hasChildNodes());
-	  console.log(root.childNodes.length);
-	  console.log(root.childNodes.item(0).nodeName);
+    root = xmlDoc.documentElement;
+   
+  if(root.hasChildNodes()) {
+    console.log(root.hasChildNodes());
+    console.log(root.childNodes.length);
+    console.log(root.childNodes.item(0).nodeName);
       console.log(root.childNodes.item(0).attributes.item(0).value); 
-	  console.log(root.childNodes.item(0).attributes.item(1).name); 
-	  console.log(root.childNodes.item(0).attributes.item(1).value); 
-	}
-	
+    console.log(root.childNodes.item(0).attributes.item(1).name); 
+    console.log(root.childNodes.item(0).attributes.item(1).value); 
+  }
+  
     var atrs = {};
-	if (root.hasAttributes()) {
+  if (root.hasAttributes()) {
       for (x=0; x < root.attributes.length; x++) {
-	    atrs[root.attributes.item(x).name] = root.attributes.item(x).value;
-	  }
-	}
-	
-	var children = {};
-	
-	if(root.hasChildNodes()) {
-	  for(x=0; x < root.childNodes.length; x++) {
-	    children[root.childNodes.item(x).nodeName] = root.childNodes.item(x);
-	  }
-	}
-	
-	return {
-	   name: root.nodeName,
-	   children: children,
-	   attributes: atrs
-	};
+      atrs[root.attributes.item(x).name] = root.attributes.item(x).value;
+    }
+  }
+  
+  var children = {};
+  
+  if(root.hasChildNodes()) {
+    for(x=0; x < root.childNodes.length; x++) {
+      children[root.childNodes.item(x).nodeName] = root.childNodes.item(x);
+    }
+  }
+  
+  return {
+     name: root.nodeName,
+     children: children,
+     attributes: atrs
+  };
   }
   
   that.parse = parse;
