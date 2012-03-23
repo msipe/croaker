ce8a61e69cc40544afee4120a88565fe20b0d961
Andy Sipe <ajs.general@gmail.com>
2012-03-23 07:29:58 -0400

    formatting clean up

--- src/croaker/js/croaker.js
+++ src/croaker/js/croaker.js
@@ -5,39 +5,37 @@ function Parser() {
     var domparser = new DOMParser(), xmlDoc = domparser.parseFromString(string,"text/xml"),   
     root = xmlDoc.documentElement;
    
-  if(root.hasChildNodes()) {
-    console.log(root.hasChildNodes());
-    console.log(root.childNodes.length);
-    console.log(root.childNodes.item(0).nodeName);
+    if(root.hasChildNodes()) {
+      console.log(root.hasChildNodes());
+      console.log(root.childNodes.length);
+      console.log(root.childNodes.item(0).nodeName);
       console.log(root.childNodes.item(0).attributes.item(0).value); 
-    console.log(root.childNodes.item(0).attributes.item(1).name); 
-    console.log(root.childNodes.item(0).attributes.item(1).value); 
-  }
+      console.log(root.childNodes.item(0).attributes.item(1).name); 
+      console.log(root.childNodes.item(0).attributes.item(1).value); 
+    }
   
     var atrs = {};
-  if (root.hasAttributes()) {
-      for (x=0; x < root.attributes.length; x++) {
-      atrs[root.attributes.item(x).name] = root.attributes.item(x).value;
+    if (root.hasAttributes()) {
+        for (x=0; x < root.attributes.length; x++) {
+        atrs[root.attributes.item(x).name] = root.attributes.item(x).value;
+      }
     }
-  }
   
-  var children = {};
+    var children = {};
   
-  if(root.hasChildNodes()) {
-    for(x=0; x < root.childNodes.length; x++) {
-      children[root.childNodes.item(x).nodeName] = root.childNodes.item(x);
+    if(root.hasChildNodes()) {
+      for(x=0; x < root.childNodes.length; x++) {
+        children[root.childNodes.item(x).nodeName] = root.childNodes.item(x);
+      }
     }
-  }
   
-  return {
-     name: root.nodeName,
-     children: children,
-     attributes: atrs
-  };
+    return {
+       name: root.nodeName,
+       children: children,
+       attributes: atrs
+    };
   }
   
   that.parse = parse;
-  
   return that;
-
 }
