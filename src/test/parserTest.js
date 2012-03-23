a6b112f4ea125a2592e7e8aed7430e068a276e4d
Morgan Sipe <morgan.talk@gmail.com>
2012-03-23 08:24:27 -0400

    reformatting tabs + jsLint operations

--- src/test/parserTest.js
+++ src/test/parserTest.js
@@ -1,41 +1,41 @@
 TestCase("Croaker.ParserTest.Tests", {
   testSimpleParseWithEmpty: function() {
     var parser = new Parser();
-	
-	var tree = parser.parse('<?xml version="1.0"?><players></players>');
-	assertThat(tree.name, equalTo('players'));
-	assertThat(tree.children, object());
-	assertThat(tree.attributes, object());
+  
+  var tree = parser.parse('<?xml version="1.0"?><players></players>');
+  assertThat(tree.name, equalTo('players'));
+  assertThat(tree.children, object());
+  assertThat(tree.attributes, object());
   },
 
   testSimpleParseWithAttributes: function() {
     var parser = new Parser();
-	
-	var tree = parser.parse('<?xml version="1.0"?><players name="bob"></players>');
-	assertThat(tree.name, equalTo('players'));
-	assertThat(tree.children, object());
-	assertThat(tree.attributes, hasMember('name', 'bob'));
+  
+  var tree = parser.parse('<?xml version="1.0"?><players name="bob"></players>');
+  assertThat(tree.name, equalTo('players'));
+  assertThat(tree.children, object());
+  assertThat(tree.attributes, hasMember('name', 'bob'));
   }, 
   
   testParseWithThreeAttributes: function() {
     var parser = new Parser();
-	
-	var tree = parser.parse('<?xml version="1.0"?><players name="bob" age="5" gender="male"></players>');
-	
-	assertThat(tree.name, equalTo('players'));
-	assertThat(tree.children, object());
-	assertThat(tree.attributes, hasMember('name', 'bob'));
-	assertThat(tree.attributes, hasMember('age', '5'));
-	assertThat(tree.attributes, hasMember('gender', 'male'));
+  
+  var tree = parser.parse('<?xml version="1.0"?><players name="bob" age="5" gender="male"></players>');
+  
+  assertThat(tree.name, equalTo('players'));
+  assertThat(tree.children, object());
+  assertThat(tree.attributes, hasMember('name', 'bob'));
+  assertThat(tree.attributes, hasMember('age', '5'));
+  assertThat(tree.attributes, hasMember('gender', 'male'));
   },
 
   testParseWithAttributesAndChildren:  function() {
     var parser = new Parser();
-	
-	var tree = parser.parse('<?xml version="1.0"?><players name="bob"><stats type="catches" value="3"/></players>');
+  
+  var tree = parser.parse('<?xml version="1.0"?><players name="bob"><stats type="catches" value="3"/></players>');
     
-	assertThat(tree.children, hasMember('stats', 'pop'));
-	
+  assertThat(tree.children, hasMember('stats', 'pop'));
+  
   },
   
   setUp: function () {
