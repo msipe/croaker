2013767c89af861e9c974c5468b167d9e9656c4e
Morgan Sipe <morgan.talk@gmail.com>
2012-03-26 09:12:54 -0400

    beginning complex tests for parser

--- src/test/parserTest.js
+++ src/test/parserTest.js
@@ -9,6 +9,15 @@ TestCase("Croaker.ParserTest.Tests", {
     assertThat(tree.attributes, object());
   },
   
+  testComplexParses: function() {
+    var parser = new croaker.Parser(),
+      tree = parser.parse('<?xml version="1.0"?><bballteams><team name="Thunder"><player name="Durant"><stat name="Average Points" value="10"/><stat name="Rebounds" value="25"/></player></team></bballteams>');
+
+    assertThat(tree.name, equalTo('bballteams'));
+  },
+  
+  
+  
   setUp: function () {
     JsHamcrest.Integration.JsTestDriver();
   }
