e048218c1e17fdfbd2d39172ca0d33eade4398e6
Morgan Sipe <morgan.talk@gmail.com>
2012-03-26 09:04:32 -0400

    fleshed out children tests

--- src/test/childrenTest.js
+++ src/test/childrenTest.js
@@ -1,12 +1,23 @@
 TestCase("Croaker.ChildrenTest.Tests", {
- 
+  
+  testChildren: function() {
+    var parser = new croaker.Parser(),
+      tree = parser.parse('<?xml version="1.0"?><players><stats/><powers/></players>');
+    
+    assertThat(tree.children.length, equalTo(2));
+    assertThat(tree.children[0].name, equalTo('stats'));
+    assertThat(tree.children[1].name, equalTo('powers'));
+  
+  },
+  
+  
   testParseWithAttributesAndChildren:  function() {
     var parser = new croaker.Parser(),
       tree = parser.parse('<?xml version="1.0"?><players name="bob"><stats type="catches" value="3"/></players>');
     
     assertThat(tree.name, equalTo('players'));
     assertThat(tree.attributes.name, equalTo('bob'));
-    assertThat(tree.children.length, equalTo(1))  
+    assertThat(tree.children.length, equalTo(1));  
     assertThat(tree.children[0].name, equalTo('stats'));
   },
   
