c2e635eed768fef2c4d9f690e021d45600ff0cd1
Morgan Sipe <morgan.talk@gmail.com>
2012-03-30 11:13:55 -0400

    added member tests

--- src/test/mapping/MappingTest.js
+++ src/test/mapping/MappingTest.js
@@ -32,7 +32,6 @@ TestCase("Croaker.Mapping.Tests", {
     assertThat(module.namespaces[0].name, 'Sample.Core' );
     assertThat(module.namespaces[0].metrics[0].name, 'MaintainabilityIndex' );
     assertThat(module.namespaces[0].metrics[0].value, 10 );
-    
   },
   
   testTypeMapping: function() {
@@ -43,7 +42,16 @@ TestCase("Croaker.Mapping.Tests", {
     assertThat(module.namespaces[0].types[0].name, 'SampleType.Core');
     assertThat(module.namespaces[0].types[0].metrics[0].name, 'Mantain');
     assertThat(module.namespaces[0].types[0].metrics[2].value, 7);
+  },
+  
+  testTypeMapping: function() {
+    var mapper = new croaker.Mapper(), module;
+      
+    module = mapper.map(this.entry);
     
+    assertThat(module.namespaces[0].types[0].members[0].name, 'Thisis.Core');
+    //assertThat(module.namespaces[0].types[0].metrics[0].name, 'Mantain');
+    //assertThat(module.namespaces[0].types[0].metrics[2].value, 7);
   },
   
   setUp: function () {
@@ -72,6 +80,15 @@ TestCase("Croaker.Mapping.Tests", {
                             new croaker.NodeEntry('Metric', {Name:'Mantain', Value:'11'}, []),
                             new croaker.NodeEntry('Metric', {Name:'CyclomaticComplexifail', Value:'42'}, []),
                             new croaker.NodeEntry('Metric', {Name:'BadabaBadabioom', Value:'7'}, [])
+                          ]),
+                          new croaker.NodeEntry('Members', {}, [
+                            new croaker.NodeEntry('Member', {Name: 'Thisis.Core'}, [
+                              new croaker.NodeEntry('Metrics', {}, [
+                                new croaker.NodeEntry('Metric', {Name:'metricly', Value:'43'}, []),
+                                new croaker.NodeEntry('Metric', {Name:'FoulPlay', Value:'98'}, []),
+                                new croaker.NodeEntry('Metric', {Name:'Rajo-Rajey', Value:'793'}, [])
+                              ])
+                            ])
                           ])
                         ])
                       ])
