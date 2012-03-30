c3b8586087cbd8548da9b73890a0fbf059023e36
Morgan Sipe <morgan.talk@gmail.com>
2012-03-30 08:40:34 -0400

    set up type mapping tests

--- src/test/mapping/MappingTest.js
+++ src/test/mapping/MappingTest.js
@@ -91,7 +91,48 @@ TestCase("Croaker.Mapping.Tests", {
      assertThat(module.namespace[0].metrics[0].value, 10 );
     
   },
-
+  
+  testTypeMapping: function() {
+    var mapper = new croaker.Mapper(),
+      entry = new croaker.NodeEntry('CodeMetricsReport', {}, [
+        new croaker.NodeEntry('Targets', {}, [
+          new croaker.NodeEntry('Target', {}, [
+            new croaker.NodeEntry('Modules', {}, [
+              new croaker.NodeEntry('Module', {Name:'Some.dll', AssemblyVersion:'1.0.2.3'}, [
+                new croaker.NodeEntry('Metrics', {}, [
+                  new croaker.NodeEntry('Metric', {Name:'MaintainabilityIndex', Value:'1'}, []),
+                  new croaker.NodeEntry('Metric', {Name:'CyclomaticComplexifail', Value:'4'}, []),
+                  new croaker.NodeEntry('Metric', {Name:'BadabaBadabioom', Value:'5'}, [])
+                ]),
+                new croaker.NodeEntry('NameSpaces', {}, [
+                  new croaker.NodeEntry('Namespace', {Name:'Sample.Core'}, [
+                    new croaker.NodeEntry('Metrics', {}, [
+                      new croaker.NodeEntry('Metric', {Name:'MaintainabilityIndex', Value:'10'}, []),
+                      new croaker.NodeEntry('Metric', {Name:'CyclomaticComplexifail', Value:'40'}, []),
+                      new croaker.NodeEntry('Metric', {Name:'BadabaBadabioom', Value:'50'}, [])
+                    ]),
+                    new croaker.NodeEntry('Types', {}, [
+                      new croaker.NodeEntry('Type', {Name: 'Sample.Core'}, [
+                        new croaker.NodeEntry('Metrics', {}, [
+                          new croaker.NodeEntry('Metric', {Name:'MaintainabilityIndex', Value:'11'}, []),
+                          new croaker.NodeEntry('Metric', {Name:'CyclomaticComplexifail', Value:'42'}, []),
+                          new croaker.NodeEntry('Metric', {Name:'BadabaBadabioom', Value:'7'}, [])
+                        ])
+                      ])
+                    ])
+                  ])
+                ])
+              ])
+            ])
+          ])
+        ])
+      ]),
+      module;
+      
+    module = mapper.map(entry);
+    
+  },
+  
   setUp: function () {
     JsHamcrest.Integration.JsTestDriver();
   }
