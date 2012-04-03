cf49c9a94ad1e7d998e805b14efe1b582259019c
Morgan Sipe <morgan.talk@gmail.com>
2012-04-03 09:13:46 -0400

    set up tests for the parser to weed out text nodes

---
+++ src/test/parser/ParseTextNodesTest.js
@@ -0,0 +1,19 @@
+TestCase("Croaker.ParseTextNodes.Tests", {
+  
+  testParseWithSpaces: function() {
+    var parser = new croaker.Parser(),
+      tree = parser.parse('<?xml version="1.0"?>  <bballteams><team name="Thunder">    <player name="Durant">    <stat name="Average Points" value="10"/>    <stat name="Rebounds" value="25"/> </player></team>     </bballteams>');
+
+    assertThat(tree.name, equalTo('bballteams'));
+    assertThat(tree.children.length, equalTo('1'));
+    assertThat(tree.children[0].children.length, equalTo('1'));
+    assertThat(tree.children[0].children[0].children.length, equalTo('2'));
+    assertThat(tree.children[0].children[0].children[0].name, equalTo('stat'));
+    assertThat(tree.children[0].children[0].children[1].name, equalTo('stat'));
+    assertThat(tree.children[0].children[0].children[1].attributes.name, equalTo('Rebounds'));
+  },
+ 
+  setUp: function () {
+    JsHamcrest.Integration.JsTestDriver();
+  }
+});
