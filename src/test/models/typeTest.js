3cf684b9f4dd9f5585039cb325488c689f46e8f5
Morgan Sipe <morgan.talk@gmail.com>
2012-04-16 09:35:25 -0400

    updated getFullMetric tests

--- src/test/models/typeTest.js
+++ src/test/models/typeTest.js
@@ -58,6 +58,30 @@ TestCase("Croaker.Type.Tests", {
     assertThat(actual[4].value, croaker.MISSING_METRIC_VALUE);
     assertThat(actual[4].getFormattedValue(), '');
   },
+  
+  testGetSingleMetric: function() {
+    var type = new croaker.Type('Joe', [], [new croaker.Metric('MaintainabilityIndex', 32), new croaker.Metric('CyclomaticComplexity', 2), new croaker.Metric('ClassCoupling', 3), new croaker.Metric('LinesOfCode', 5)])
+      actual = type.getFullMetrics();
+    
+    assertThat(actual.length, 5);
+    
+    assertThat(actual[0].name, 'MaintainabilityIndex');
+    assertThat(actual[0].value, 32);
+    
+    assertThat(actual[1].name, 'CyclomaticComplexity');
+    assertThat(actual[1].value, 2);
+
+    
+    assertThat(actual[2].name, 'ClassCoupling');
+    assertThat(actual[2].value, 3);
+    
+    assertThat(actual[3].name, 'DepthOfInheritance');
+    assertThat(actual[3].value, croaker.MISSING_METRIC_VALUE);
+    assertThat(actual[3].getFormattedValue(), '');
+    
+    assertThat(actual[4].name, 'LinesOfCode');
+    assertThat(actual[4].value, 5);
+  },
 
   setUp: function () {
     JsHamcrest.Integration.JsTestDriver();
