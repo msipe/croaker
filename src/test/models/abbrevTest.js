21128f8d76c9e4f91adb95b12b49be27a2330be6
Morgan Sipe <morgan.talk@gmail.com>
2012-04-23 09:27:14 -0400

    added tests for metric definitions

--- src/test/models/abbrevTest.js
+++ src/test/models/abbrevTest.js
@@ -1,7 +1,12 @@
 TestCase("Croaker.NameAbbreviation.Tests", {
   testAbbrev: function() {
-    var metrics = [], member = new croaker.Member('joe','iceCream.js',3, metrics);
+    var a = new croaker.MetricDefinition('a', 'ay') ;
     
+    assertThat(a.abbreviation, 'a');
+    assertThat(croaker.LinesOfCode.abbreviation, 'LC');
+    assertThat(croaker.CyclomaticComplexity.abbreviation, 'CyC');
+    assertThat(croaker.ClassCoupling.abbreviation, 'CC');
+    assertThat(croaker.DepthOfInheritance.abbreviation, 'DI');
   },
   
 
