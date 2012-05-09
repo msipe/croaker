0efaffc468d64a6d5e7602f8f696123e958900bc
Morgan Sipe <morgan.talk@gmail.com>
2012-05-09 14:40:27 -0400

    set up filter error tests

---
+++ src/test/filters/FilterErrorTest.js
@@ -0,0 +1,83 @@
+TestCase("Croaker.FilterError.Tests", {
+  
+  testEmptyFilterFunctonality: function() {
+    var mapper = new croaker.Mapper(), module = mapper.map(this.entryMultipleNS),
+      filteredList = new croaker.FilteredList(module);
+      
+  },
+  
+  setUp: function () {
+    JsHamcrest.Integration.JsTestDriver();
+        
+    this.entryMultipleNS = new croaker.NodeEntry('CodeMetricsReport', {}, [
+      new croaker.NodeEntry('Targets', {}, [
+        new croaker.NodeEntry('Target', {}, [
+          new croaker.NodeEntry('Modules', {}, [
+            new croaker.NodeEntry('Module', {Name:'Some.dll', AssemblyVersion:'1.0.2.3'}, [
+              new croaker.NodeEntry('Metrics', {}, [
+                  ]),
+                  new croaker.NodeEntry('NameSpaces', {}, [
+                    new croaker.NodeEntry('Namespace', {Name:'Sample.Core'}, [
+                      new croaker.NodeEntry('Metrics', {}, [
+                      ]),
+                      new croaker.NodeEntry('Types', {}, [
+                        new croaker.NodeEntry('Type', {Name: 'SampleType.Core'}, [
+                          new croaker.NodeEntry('Metrics', {}, [
+                          ]),
+                          new croaker.NodeEntry('Members', {}, [])
+                        ]),
+                        new croaker.NodeEntry('Type', {Name: 'SecondType.Core'}, [
+                          new croaker.NodeEntry('Metrics', {}, [
+                          ]),
+                          new croaker.NodeEntry('Members', {}, [])
+                        ])
+                      ])
+                    ]),
+                    new croaker.NodeEntry('Namespace', {Name:'Other.Core'}, [
+                      new croaker.NodeEntry('Metrics', {}, [
+                      ]),
+                      new croaker.NodeEntry('Types', {}, [
+                        new croaker.NodeEntry('Type', {Name: 'OtherType.Core'}, [
+                          new croaker.NodeEntry('Metrics', {}, [
+                          ]),
+                          new croaker.NodeEntry('Members', {}, [
+                            new croaker.NodeEntry('Member', {Name: 'Thisis.Core', File:'50982.jkalhfksdl', Line:'5'}, [
+                              new croaker.NodeEntry('Metrics', {}, [
+                              ])
+                            ]),
+                            new croaker.NodeEntry('Member', {Name: 'weard.Core', File:'50982.jkalhfksdl', Line:'5'}, [
+                              new croaker.NodeEntry('Metrics', {}, [
+                              ])
+                            ])
+                          ])
+                        ])
+                      ])
+                    ]),
+                    new croaker.NodeEntry('Namespace', {Name:'Third.Core'}, [
+                      new croaker.NodeEntry('Metrics', {}, [
+                      ]),
+                      new croaker.NodeEntry('Types', {}, [
+                        new croaker.NodeEntry('Type', {Name: 'SomeType.Core'}, [
+                          new croaker.NodeEntry('Metrics', {}, [
+                          ]),
+                          new croaker.NodeEntry('Members', {}, [
+                            new croaker.NodeEntry('Member', {Name: 'Thisis.Core', File:'50982.jkalhfksdl', Line:'5'}, [
+                              new croaker.NodeEntry('Metrics', {}, [
+                              ])
+                            ]),
+                            new croaker.NodeEntry('Member', {Name: 'weard.Core', File:'50982.jkalhfksdl', Line:'5'}, [
+                              new croaker.NodeEntry('Metrics', {}, [
+                              ])
+                            ])
+                          ])
+                        ])
+                      ])
+                    ])
+                  ])
+                ])
+              ])
+            ])
+          ])
+        ]);
+  }
+});
