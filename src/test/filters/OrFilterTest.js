b11da13e4e0b9a523dde69b3a3e6c164043266e4
Morgan Sipe <morgan.talk@gmail.com>
2012-06-06 15:11:23 -0400

    added orFilter tests for arrays and orFilters

--- src/test/filters/OrFilterTest.js
+++ src/test/filters/OrFilterTest.js
@@ -13,6 +13,37 @@ TestCase("Croaker.OrFilter.Tests", {
       assertThat(orFilter.filter(''), true);
   },
   
+  testMultipleOrFilterElementsReturnTrue: function() {
+    var filters = [
+      mock(croaker.StrainFilter),
+      mock(croaker.StrainFilter),
+      mock(croaker.StrainFilter)
+      ],
+      orFilter = new croaker.OrFilter(filters);
+      
+    when(filters[0]).filter('').thenReturn(true);
+    when(filters[1]).filter('').thenReturn(true);
+    when(filters[2]).filter('').thenReturn(true);
+    
+    assertThat(orFilter.filter(''), true);
+  },
+  
+  testMultipleOrFilterElementsReturnTrueRegardlessOfFalseElement: function() {
+    var filters = [
+      mock(croaker.StrainFilter),
+      mock(croaker.StrainFilter),
+      mock(croaker.StrainFilter)
+      ],
+      orFilter = new croaker.OrFilter(filters);
+      
+    when(filters[0]).filter('').thenReturn(true);
+    when(filters[1]).filter('').thenReturn(false);
+    when(filters[2]).filter('').thenReturn(true);
+    
+    assertThat(orFilter.filter(''), true);
+  },
+  
+  
    setUp: function () {
     JsHamcrest.Integration.JsTestDriver();
     JsMockito.Integration.JsTestDriver();
