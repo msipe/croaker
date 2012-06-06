d01f9a9581bdfbb8b598318542a4613efae9d4e9
Morgan Sipe <morgan.talk@gmail.com>
2012-06-06 11:35:33 -0400

    added emptyFilter support to the AndFilterTests

--- src/test/filters/AndFilterTest.js
+++ src/test/filters/AndFilterTest.js
@@ -1,10 +1,13 @@
 TestCase("Croaker.AndFilter.Tests", {
   
    testEmptyFilterGivesTrue: function() {
-
-  },
+     var andFilter = new croaker.AndFilter([]);
+     
+     assertThat(andFilter.filter(), true);
+   },
   
    setUp: function () {
     JsHamcrest.Integration.JsTestDriver();
+    JsMockito.Integration.JsTestDriver();
   }
 });
