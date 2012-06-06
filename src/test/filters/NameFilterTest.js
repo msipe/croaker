c486cb628d3cf64494abcf4aa65fcb9b207fa77e
Morgan Sipe <morgan.talk@gmail.com>
2012-06-06 11:23:25 -0400

    updated NameFilterTest

--- src/test/filters/NameFilterTest.js
+++ src/test/filters/NameFilterTest.js
@@ -7,12 +7,7 @@ TestCase("Croaker.NameFilter.Tests", {
     assertThat(filter.filter({name: 'Thisis.Core'}), true);
     assertThat(filter.filter({name: 'abcThisis.Core'}), false);
   },
-  
-  testMultipleNameFilters() {
-  
-  
-  }
-  
+
    setUp: function () {
     JsHamcrest.Integration.JsTestDriver();
   }
