af5660360019159433523e4c4529e66fdc404288
Morgan Sipe <morgan.talk@gmail.com>
2012-06-06 11:19:07 -0400

    moved nameFilter test to its own file

---
+++ src/test/filters/NameFilterTest.js
@@ -0,0 +1,14 @@
+TestCase("Croaker.NameFilter.Tests", {
+  
+   testDefualts: function() {
+    var filter = new croaker.NameFilter('^Thisis.Core');
+    
+    assertThat(filter.filter({name: 'a'}), false);
+    assertThat(filter.filter({name: 'Thisis.Core'}), true);
+    assertThat(filter.filter({name: 'abcThisis.Core'}), false);
+  },
+  
+   setUp: function () {
+    JsHamcrest.Integration.JsTestDriver();
+  }
+});
