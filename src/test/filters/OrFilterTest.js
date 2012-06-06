2685036f787dc5771a2f75cb8b82a86715ba1e9c
Morgan Sipe <morgan.talk@gmail.com>
2012-06-06 15:05:28 -0400

    added test for OrFilter w/true element

--- src/test/filters/OrFilterTest.js
+++ src/test/filters/OrFilterTest.js
@@ -3,9 +3,15 @@ TestCase("Croaker.OrFilter.Tests", {
     var orFilter = new croaker.OrFilter([]);
     
     assertThat(orFilter.filter(''), true);
-  
   },
   
+  testSingleOrFilterElementReturnsTrue: function() {
+    var filter = mock(croaker.StrainFilter),
+      orFilter = new croaker.OrFilter([filter]);
+      
+      when(filter).filter('').thenReturn(true);
+      assertThat(orFilter.filter(''), true);
+  },
   
    setUp: function () {
     JsHamcrest.Integration.JsTestDriver();
