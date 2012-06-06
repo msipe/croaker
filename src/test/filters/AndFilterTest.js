79ffc9458e167e430f3153e40015bb4463ac5378
Morgan Sipe <morgan.talk@gmail.com>
2012-06-06 12:02:58 -0400

    added single elemetn test to the andFilter test

--- src/test/filters/AndFilterTest.js
+++ src/test/filters/AndFilterTest.js
@@ -5,6 +5,13 @@ TestCase("Croaker.AndFilter.Tests", {
      
      assertThat(andFilter.filter(), true);
    },
+   
+   testSingleTrueFilter: function() {
+     var strainFilter = {filter: function() {return true}},
+       andFilter = new croaker.AndFilter([]);
+      
+     assertThat(andFilter.filter(''), true);
+   },
   
    setUp: function () {
     JsHamcrest.Integration.JsTestDriver();
