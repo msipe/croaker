b4f4f1a60fca11c4325022ff57dbe96190ac8560
Morgan Sipe <morgan.talk@gmail.com>
2012-06-07 10:49:08 -0400

    added a test for no filters

--- src/test/filters/FilteredListTest.js
+++ src/test/filters/FilteredListTest.js
@@ -43,6 +43,19 @@ TestCase("Croaker.FilteredList.Tests", {
       
       assertThat(list.getAccepted(), [1, 2, 3]);
   },
+  
+  testNoFilters: function() {
+    var module = mock(croaker.Module), 
+      filters = [], list;
+      
+      when(module).flatten().thenReturn([1, 2, 3]);
+    
+      list = new croaker.FilteredList(module);
+      
+      list.applyFilters(filters);
+      
+      assertThat(list.getAccepted(), [1, 2, 3]);
+  },
 
    setUp: function () {
     JsHamcrest.Integration.JsTestDriver();
